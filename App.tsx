ec981555f7750c00687229eadca974c413bf64be
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-07-29 21:34:53 -0300

    feat: config expo font

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
+import {
+    Roboto_400Regular,
+    Roboto_700Bold,
+    useFonts,
+} from "@expo-google-fonts/roboto";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Roboto_700Bold,
+        Roboto_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
