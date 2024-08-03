c382ef54fdb4c1ea6bee1e33f6832938cde7ef54
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-08-03 14:11:28 -0300

    feat: create variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,23 +8,50 @@ import { ComponentProps } from "react";
 type ButtonProps = ComponentProps<typeof GluestackButton> & {
     title: string;
     isLoading?: boolean;
+    variant?: "solid" | "outline";
 };
 
-export function Button({ title, isLoading, ...props }: ButtonProps) {
+export function Button({
+    title,
+    isLoading,
+    variant = "solid",
+    ...props
+}: ButtonProps) {
+    const variants = {
+        solid: {
+            bg: "$green700",
+            borderWidth: "$0",
+            borderColor: "$green500",
+            "$active-bg": "$green500",
+            color: "$white",
+        } as any,
+        outline: {
+            bg: "transparent",
+            borderWidth: "$1",
+            borderColor: "$green500",
+            "$active-bg": "$gray500",
+            color: "$green500",
+        },
+    };
+
     return (
         <GluestackButton
             w="$full"
             h="$14"
-            bg="$green700"
-            borderWidth="$0"
-            borderColor="$green500"
+            bg={variants[variant].bg}
+            borderWidth={variants[variant].borderWidth}
+            borderColor={variants[variant].borderColor}
             rounded="$sm"
-            $active-bg="$green500"
+            $active-bg={variants[variant]["$active-bg"]}
             disabled={isLoading}
             {...props}
         >
             {!isLoading ? (
-                <Text color="$white" fontFamily="$heading" fontSize="$sm">
+                <Text
+                    color={variants[variant].color}
+                    fontFamily="$heading"
+                    fontSize="$sm"
+                >
                     {title}
                 </Text>
             ) : (
