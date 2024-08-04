a8fd8964b132068129a9ef16f88fa731c98ce8c0
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-08-03 22:28:22 -0300

    feat: header title

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 
 import { GroupList } from "./GroupList";
 import { HomeHeader } from "./HomeHeader";
@@ -7,7 +7,17 @@ export function Home() {
     return (
         <VStack>
             <HomeHeader />
+
             <GroupList />
+
+            <VStack px="$8">
+                <HStack alignItems="center" justifyContent="space-between">
+                    <Heading color="$gray200">Exercícios</Heading>
+                    <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+                        4
+                    </Text>
+                </HStack>
+            </VStack>
         </VStack>
     );
 }
