86049028083d11d60b7425df1462cf537f8f8d26
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-08-05 20:27:28 -0300

    feat: create exercise card

---
+++ src/components/screens/Exercise/ExerciseCard.tsx
@@ -0,0 +1,55 @@
+import {
+    Heading,
+    HStack,
+    Icon,
+    Image,
+    Text,
+    VStack,
+} from "@gluestack-ui/themed";
+import { ChevronRight } from "lucide-react-native";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+export type ExerciseProps = TouchableOpacityProps;
+
+export function ExerciseCard({ ...props }: ExerciseProps) {
+    return (
+        <TouchableOpacity {...props}>
+            <HStack
+                bg="$gray500"
+                alignItems="center"
+                p="$2"
+                pr="$4"
+                rounded="$md"
+                mb="$3"
+            >
+                <Image
+                    source={{
+                        uri: "https://www.fiqueinforma.com/wp-content/uploads/2008/12/puxadas-m%C3%BAsculos.png",
+                    }}
+                    alt="Imagem do exercicio"
+                    w="$16"
+                    h="$16"
+                    rounded="$md"
+                    mr="$4"
+                    resizeMode="cover"
+                />
+
+                <VStack flex={1}>
+                    <Heading fontSize="$lg" color="$white">
+                        Puxada frontal
+                    </Heading>
+                    <Text
+                        fontSize="$sm"
+                        color="$gray200"
+                        mt="$1"
+                        numberOfLines={2}
+                    >
+                        3 séries x 12 repetições
+                    </Text>
+                </VStack>
+
+                <Icon as={ChevronRight} color="$gray300" size="xl" />
+            </HStack>
+        </TouchableOpacity>
+    );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,44 @@
-import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList, Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 
+import { ExerciseCard } from "@components/screens/Exercise";
 import { GroupList, HomeHeader } from "@components/screens/Home";
 
 export function Home() {
+    const [exercises, setExercises] = useState([
+        "Puxada frontal",
+        "Remada unilateral",
+        "Levantamento terra",
+        "Agachamento",
+        "Pulley",
+        "Elevação lateral",
+        "Corda horizontal",
+        "Corda vertical",
+        "Cadeira extensora",
+        "Cadeira flexora",
+        "Mesa flexora",
+    ]);
     return (
-        <VStack>
+        <VStack flex={1}>
             <HomeHeader />
 
             <GroupList />
 
-            <VStack px="$8">
+            <VStack px="$8" flex={1}>
                 <HStack alignItems="center" justifyContent="space-between">
                     <Heading color="$gray200">Exercícios</Heading>
                     <Text color="$gray200" fontSize="$sm" fontFamily="$body">
-                        4
+                        {exercises.length}
                     </Text>
                 </HStack>
+
+                <FlatList
+                    data={exercises}
+                    keyExtractor={(item) => String(item)}
+                    renderItem={({ item }) => <ExerciseCard />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 20 }}
+                />
             </VStack>
         </VStack>
     );
