8933441b52ecc67f733c6758f3cf65866f62845a
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-08-06 19:40:29 -0300

    fix: size prop

--- src/components/UserPhoto.tsx
+++ src/components/UserPhoto.tsx
@@ -1,19 +1,18 @@
 import { Avatar, AvatarFallbackText, AvatarImage } from "@gluestack-ui/themed";
+import { ComponentProps } from "react";
 
-type UserPhotoProps = {
+type UserPhotoProps = ComponentProps<typeof Avatar> & {
     uri: string;
 };
 
-export function UserPhoto({ uri }: UserPhotoProps) {
+export function UserPhoto({ uri, size = "lg", ...props }: UserPhotoProps) {
     return (
-        <Avatar size="lg">
+        <Avatar size={size} {...props}>
             <AvatarFallbackText>Wilson Junior</AvatarFallbackText>
             <AvatarImage
                 source={{
                     uri,
                 }}
-                w="$16"
-                h="$16"
                 borderWidth="$2"
                 borderColor="$gray400"
                 alt="Imagem do usuário"
