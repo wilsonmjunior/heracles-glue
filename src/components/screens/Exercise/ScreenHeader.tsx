e27d5a79691bc9fa599536141248d9038338e99f
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-08-16 21:37:00 -0300

    fix: lint

--- src/components/screens/Exercise/ScreenHeader.tsx
+++ src/components/screens/Exercise/ScreenHeader.tsx
@@ -6,34 +6,34 @@ import { ArrowLeft } from "lucide-react-native";
 import BodySvg from "@assets/body.svg";
 
 export function ScreenHeader() {
-    const navigation = useNavigation();
+	const navigation = useNavigation();
 
-    function handleGoBack() {
-        navigation.goBack();
-    }
+	function handleGoBack() {
+		navigation.goBack();
+	}
 
-    return (
-        <VStack bg="$gray600" px="$8" pt="$12">
-            <TouchableOpacity onPress={handleGoBack}>
-                <Icon as={ArrowLeft} color="$green500" size="xl" />
-            </TouchableOpacity>
+	return (
+		<VStack bg="$gray600" px="$8" pt="$12">
+			<TouchableOpacity onPress={handleGoBack}>
+				<Icon as={ArrowLeft} color="$green500" size="xl" />
+			</TouchableOpacity>
 
-            <HStack
-                mt="$3"
-                alignItems="center"
-                justifyContent="space-between"
-                mb="$8"
-            >
-                <Heading color="$gray100" fontSize="$lg" flexShrink={1}>
-                    Puxada frontal
-                </Heading>
-                <HStack gap="$1" alignItems="center">
-                    <BodySvg />
-                    <Text color="$gray200" textTransform="capitalize">
-                        Costas
-                    </Text>
-                </HStack>
-            </HStack>
-        </VStack>
-    );
+			<HStack
+				mt="$3"
+				alignItems="center"
+				justifyContent="space-between"
+				mb="$8"
+			>
+				<Heading color="$gray100" fontSize="$lg" flexShrink={1}>
+					Puxada frontal
+				</Heading>
+				<HStack gap="$1" alignItems="center">
+					<BodySvg />
+					<Text color="$gray200" textTransform="capitalize">
+						Costas
+					</Text>
+				</HStack>
+			</HStack>
+		</VStack>
+	);
 }
--- src/components/screens/History/HistoryCard.tsx
+++ src/components/screens/History/HistoryCard.tsx
@@ -1,31 +1,31 @@
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 
 export function HistoryCard() {
-    return (
-        <HStack
-            bgColor="$gray600"
-            px="$5"
-            py="$4"
-            mb="$3"
-            alignItems="center"
-            justifyContent="space-between"
-            rounded="$lg"
+  return (
+    <HStack
+      bgColor="$gray600"
+      px="$5"
+      py="$4"
+      mb="$3"
+      alignItems="center"
+      justifyContent="space-between"
+      rounded="$lg"
+    >
+      <VStack flex={1} gap="$1" mr="$5">
+        <Heading
+          color="$white"
+          fontSize="$md"
+          textTransform="capitalize"
+          numberOfLines={1}
         >
-            <VStack flex={1} gap="$1" mr="$5">
-                <Heading
-                    color="$white"
-                    fontSize="$md"
-                    textTransform="capitalize"
-                    numberOfLines={1}
-                >
-                    Costas
-                </Heading>
-                <Text color="$gray100" fontSize="$lg" numberOfLines={1}>
-                    Puxada frontal
-                </Text>
-            </VStack>
+          Costas
+        </Heading>
+        <Text color="$gray100" fontSize="$lg" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
 
-            <Text color="$gray300">08:56</Text>
-        </HStack>
-    );
+      <Text color="$gray300">08:56</Text>
+    </HStack>
+  );
 }
