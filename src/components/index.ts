b93287f5213a834d1424da68e22f571e3efb30fc
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-08-16 21:36:15 -0300

    fix: export components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { Button } from "./Button";
+export { DataList } from "./DataList";
+export { Group } from "./Group";
+export { Input } from "./Input";
+export { Loading } from "./Loading";
+export { Page } from "./Page";
+export { ScreenHeader } from "./ScreenHeader";
+export { UserPhoto } from "./UserPhoto";
