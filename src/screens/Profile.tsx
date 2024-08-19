5dbf3df59b0c074af50e99912403610a666df6ce
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-08-18 21:12:10 -0300

    feat: change profile photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,75 +1,106 @@
+import { useState } from "react";
+import { ScrollView, TouchableOpacity } from "react-native";
+import { Center, Heading, Text, View, VStack } from "@gluestack-ui/themed";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, Text, View, VStack } from "@gluestack-ui/themed";
-import { ScrollView, TouchableOpacity } from "react-native";
+import { useToast } from "../context/toast";
 
 export function Profile() {
-    return (
-        <VStack flex={1}>
-            <ScreenHeader title="Perfil" />
+  const [loading, setLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/wilsonmjunior.png",
+  );
 
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <Center mt="$6" px="$10">
-                    <UserPhoto
-                        uri="https://github.com/wilsonmjunior.png"
-                        size="2xl"
-                    />
+  const toast = useToast();
 
-                    <View mt="$2" mb="$8">
-                        <TouchableOpacity>
-                            <Text
-                                fontSize="$md"
-                                fontFamily="$heading"
-                                color="$green500"
-                            >
-                                Alterar Foto
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
+  async function handleChangePhoto() {
+    try {
+      setLoading(true);
 
-                    <Center gap="$4">
-                        <Input placeholder="Nome" bg="$gray600" />
-                        <Input
-                            placeholder="wilson@email.com"
-                            bg="$gray600"
-                            isReadOnly
-                        />
-                    </Center>
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [2, 4],
+        allowsEditing: true,
+      });
 
-                    <>
-                        <Heading
-                            alignSelf="flex-start"
-                            color="$gray200"
-                            fontSize="$md"
-                            mt="$12"
-                            mb="$2"
-                        >
-                            Alterar senha
-                        </Heading>
+      if (photoSelected.canceled) {
+        return;
+      }
 
-                        <Center w="$full" gap="$4">
-                            <Input
-                                placeholder="Senha antiga"
-                                bg="$gray600"
-                                secureTextEntry
-                            />
-                            <Input
-                                placeholder="Nova Senha"
-                                bg="$gray600"
-                                secureTextEntry
-                            />
-                            <Input
-                                placeholder="Confirmar senha"
-                                bg="$gray600"
-                                secureTextEntry
-                            />
-                            <Button title="Atualizar" />
-                        </Center>
-                    </>
-                </Center>
-            </ScrollView>
-        </VStack>
-    );
+      const photo = photoSelected.assets[0];
+
+      if (photo.uri) {
+        const photoInfo = (await FileSystem.getInfoAsync(photo.uri)) as {
+          size: number;
+        };
+
+        if (photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+            action: "error",
+          });
+        }
+      }
+
+      setUserPhoto(photo.uri);
+    } catch (error) {
+      toast.show({ title: "Erro ao alterar imagem", action: "error" });
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Center mt="$6" px="$10">
+          <UserPhoto uri={userPhoto} isLoading={loading} size="2xl" />
+
+          <View mt="$2" mb="$8">
+            <TouchableOpacity onPress={handleChangePhoto}>
+              <Text fontSize="$md" fontFamily="$heading" color="$green500">
+                Alterar Foto
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <Center gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input placeholder="wilson@email.com" bg="$gray600" isReadOnly />
+          </Center>
+
+          <>
+            <Heading
+              alignSelf="flex-start"
+              color="$gray200"
+              fontSize="$md"
+              mt="$12"
+              mb="$2"
+            >
+              Alterar senha
+            </Heading>
+
+            <Center w="$full" gap="$4">
+              <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+              <Input placeholder="Nova Senha" bg="$gray600" secureTextEntry />
+              <Input
+                placeholder="Confirmar senha"
+                bg="$gray600"
+                secureTextEntry
+              />
+              <Button title="Atualizar" />
+            </Center>
+          </>
+        </Center>
+      </ScrollView>
+    </VStack>
+  );
 }
